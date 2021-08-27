f81c3e820dda49a31e1ed0d2ed2281a6d6805e06
nurakdur <nur.akdur@mudimedia.com>
2021-08-27 12:54:03 +0300

    user signup route and validator setup with dto

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,10 @@
 import { Controller, Post, Get, Body, Param } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
 
-@Controller('users')
+@Controller('auth')
 export class UsersController {
-    @Post() /* This decorator will allow only HTTP Post requests to this handler. */
-    createUser(@Body() body: any) {
+    @Post('/signup') /* This decorator will allow only HTTP Post requests to this handler. */
+    createUser(@Body() body: CreateUserDto) { /** As you can see here we are expecting body as CreateUserDto that means body should have email and password fields. */
         console.log(body);
     }
 
